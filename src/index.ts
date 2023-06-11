305103bdcfd2d148fd686c4e01f00be926b0ef60
valon <valonmulolli@gmail.com>
2023-06-11 15:16:55 +0200

    Passwordless, Email based Authentication

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
 import tweetRoutes from './routes/tweetRoutes'
+import authRoutes from './routes/authRoutes'
+import { authenticateToken } from './middleware/authMiddleware';
 
 const app = express();
 app.use(express.json());
-app.use('/user', userRoutes);
-app.use('/tweet', tweetRoutes);
+app.use('/user', authenticateToken,userRoutes);
+app.use('/tweet',authenticateToken, tweetRoutes);
+app.use('/auth', authRoutes);
+
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -8,19 +8,22 @@ const prisma = new PrismaClient();
 
 // Create user
 router.post('/', async (req, res) => {
-	const { content, image, userId } = req.body;
+	const { content, image } = req.body;
+	// @ts-ignore
+	const user = req.user;
 
 	try {
 		const result = await prisma.tweet.create({
 			data: {
 				content,
 				image,
-				userId,
+				userId: user.id,
 			},
+			include: { user: true },
 		});
 
 		res.json(result);
-	} catch (error) {
+	} catch (e) {
 		res.status(400).json({ error: 'Username and email should be unique' });
 	}
 });
@@ -43,27 +46,29 @@ router.get('/', async (req, res) => {
 	res.json(allTweets);
 });
 
-// get one tweet
+// get one Tweet
 router.get('/:id', async (req, res) => {
 	const { id } = req.params;
-	const tweet = await prisma.tweet.findUnique({ where: { id: Number(id) },
-	include: { user: true}
-});
+	console.log('Query tweet with id: ', id);
+
+	const tweet = await prisma.tweet.findUnique({
+		where: { id: Number(id) },
+		include: { user: true },
+	});
 	if (!tweet) {
-		return res.status(404).json({ error: 'Tweet not found' });
+		return res.status(404).json({ error: 'Tweet not found!' });
 	}
+
 	res.json(tweet);
 });
 
-// update tweet
-
+// update Tweet
 router.put('/:id', (req, res) => {
 	const { id } = req.params;
-	res.status(501).json({ error: `Not implemented: ${id}` });
+	res.status(501).json({ error: `Not Implemented: ${id}` });
 });
 
-//delete tweet
-
+// delete Tweet
 router.delete('/:id', async (req, res) => {
 	const { id } = req.params;
 	await prisma.tweet.delete({ where: { id: Number(id) } });
